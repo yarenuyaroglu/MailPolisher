dcf8cc058fb04f3f3793eec67e17034f05723318
yarenUyaroglu <uyaroglu.yaren@gmail.com>
2025-08-08 18:43:59 +0300

    Initial Commit

---
+++ Mail Polisher/Mail_PolisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mail_PolisherApp.swift
+//  Mail Polisher
+//
+//  Created by yarenuyarogluuu on 8.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Mail_PolisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
